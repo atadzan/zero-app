be13cc2a353c26c1da8378cd3490de0722f5b7b8
atajan <envader001@gmail.com>
2023-09-02 19:12:05 +0500

    minor changes

--- internal/logic/user/getByTokenLogic.go
+++ internal/logic/user/getByTokenLogic.go
@@ -24,7 +24,6 @@ func NewGetByTokenLogic(ctx context.Context, svcCtx *svc.ServiceContext) *GetByT
 }
 
 func (l *GetByTokenLogic) GetByToken() (resp *types.GetProfileRes, err error) {
-	// todo: add your logic here and delete this line
 
 	return
 }
